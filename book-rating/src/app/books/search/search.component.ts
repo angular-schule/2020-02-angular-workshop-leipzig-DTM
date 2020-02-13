b10388b24789b7d9856dab80b3854792a1371143
Ferdinand Malcher <ferdinand@malcher.media>
2020-02-13 17:16:52 +0100

    Typeahead Search

--- book-rating/src/app/books/search/search.component.ts
+++ book-rating/src/app/books/search/search.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { debounceTime, filter, distinctUntilChanged, switchMap, tap } from 'rxjs/operators';
+import { BookStoreService } from '../shared/book-store.service';
+import { Book } from '../shared/book';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'br-search',
@@ -7,9 +12,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  searchControl: FormControl;
+  results$: Observable<Book[]>;
+
+  loading = false;
+
+  constructor(private bs: BookStoreService) { }
 
   ngOnInit(): void {
+    this.searchControl = new FormControl('');
+
+    this.results$ = this.searchControl.valueChanges.pipe(
+      debounceTime(1000),
+      filter(term => term.length >= 3 || !term.length),
+      distinctUntilChanged(),
+      tap(() => this.loading = true),
+      switchMap(term => term ? this.bs.search(term) : of([])),
+      tap(() => this.loading = false),
+    );
   }
 
 }
--- book-rating/src/app/books/shared/book-store.service.ts
+++ book-rating/src/app/books/shared/book-store.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Book } from './book';
+import { of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
